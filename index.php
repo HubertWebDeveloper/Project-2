f76fe112d65a9faf5f5bff96c79a240d7b9a106f
HubertWebDeveloper <145841547+HubertWebDeveloper@users.noreply.github.com>
2023-12-25 19:07:08 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,392 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="swiper.css" />
+</head>
+<body>
+    <div class="top_nav">
+        <div class="container top_nav_container">
+            <div class="top_nav_wrapper">
+                <p class="top_nav_p">
+                 One stop website for all your 
+                 financial and lifestyle hacks. 
+                </p>
+                <a href="#" clas="top_nav_link">Only Staff Member Allowed</a>
+            </div>
+        </div>
+    </div>
+    <nav class="nav">
+        <div class="container nav_container">
+            <img src="../MyOwnerLogo.png" class="logo">
+            <a href="#" class="nav_logo">DONT MISS</a>
+            <ul class="nav_list">
+                <li class="nav_item"><a href="#" class="nav_link">Home</a></li>
+                <li class="nav_item"><a href="#" class="nav_link">About</a></li>
+                <li class="nav_item"><a href="#" class="nav_link">Contact</a></li>
+                <li class="nav_item"><a href="#" class="nav_link">Sign Up</a></li>
+            </ul>
+            <div class="nav_icons">
+                <form action="" class="nav_form">
+                    <input type="text" class="nav_input" placeholder="Search...">
+                    <a href="#"><img src="../search.png" class="nav_search"></a>
+                </form>
+                <a href="#"><img src="../cart.png" class="nav_cart"></a>
+                <a href="#"><img src="../signup.png" class="nav_signup"></a>
+                <img src="../bar.png" class="bar_icon">
+            </div>
+        </div>
+    </nav>
+    <header class="header">
+        <div class="container header_container">
+            <ul class="header_categ">
+                <h2>Top Categories</h2>
+                <a href="#" class="header_categ_links">T-shirts</a>
+                <a href="#" class="header_categ_links">Shoes</a>
+                <a href="#" class="header_categ_links">Sweater</a>
+                <a href="#" class="header_categ_links">Pinjams</a>
+                <a href="#" class="header_categ_links">Hoods</a>
+                <a href="#" class="header_categ_links">Neckles</a>
+                <a href="#" class="header_categ_links">Watches</a>
+                <a href="#" class="header_categ_links">Phones</a>
+            </ul>
+            <div class="slide">
+                <img src="../MyOwnerLogo.png" class="header_img">
+            </div>
+        </div>
+    </header>
+    <section class="section">
+        <div class="container">
+            <div class="section_category">
+                <span class="section_reactable"></span>
+                <p class="section_category_p">Today's Deal</p>
+            </div>
+            <div class="section_header">
+                <div class="count_title">
+                    <h3>Days</h3>
+                    <h3>Months</h3>
+                    <h3>Hours</h3>
+                    <h3>Seconds</h3>
+                </div>
+                <p id="demo"></p>
+            </div>
+        </div>
+    </section>
+    <section class="section">
+        <div class="container">
+           <div class="swiper mySwiper">
+              <div class="swiper-wrapper content">
+                <div class="card swiper-slide">
+                    <div class="card-content">
+                        <div class="image">
+                            <img src="../my new logo.png">
+                        </div>
+                        <div class="about">
+                            <p>For a region hit hard by poverty, 
+                                genocide and disease, 
+                                the transformation is remarkable.
+                            </p>
+                        </div>
+                        <div class="button">
+                            <button class="aboutMe">About Me</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="card swiper-slide">
+                    <div class="card-content">
+                        <div class="image">
+                            <img src="../my new logo.png">
+                        </div>
+                        <div class="about">
+                            <p>For a region hit hard by poverty, 
+                                genocide and disease, 
+                                the transformation is remarkable.
+                            </p>
+                        </div>
+                        <div class="button">
+                            <button class="aboutMe">About Me</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="card swiper-slide">
+                    <div class="card-content">
+                        <div class="image">
+                            <img src="../my new logo.png">
+                        </div>
+                        <div class="about">
+                            <p>For a region hit hard by poverty, 
+                                genocide and disease, 
+                                the transformation is remarkable.
+                            </p>
+                        </div>
+                        <div class="button">
+                            <button class="aboutMe">About Me</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="card swiper-slide">
+                    <div class="card-content">
+                        <div class="image">
+                            <img src="../my new logo.png">
+                        </div>
+                        <div class="about">
+                            <p>For a region hit hard by poverty, 
+                                genocide and disease, 
+                                the transformation is remarkable.
+                            </p>
+                        </div>
+                        <div class="button">
+                            <button class="aboutMe">About Me</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="card swiper-slide">
+                    <div class="card-content">
+                        <div class="image">
+                            <img src="../my new logo.png">
+                        </div>
+                        <div class="about">
+                            <p>For a region hit hard by poverty, 
+                                genocide and disease, 
+                                the transformation is remarkable.
+                            </p>
+                        </div>
+                        <div class="button">
+                            <button class="aboutMe">About Me</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="card swiper-slide">
+                    <div class="card-content">
+                        <div class="image">
+                            <img src="../my new logo.png">
+                        </div>
+                        <div class="about">
+                            <p>For a region hit hard by poverty, 
+                                genocide and disease, 
+                                the transformation is remarkable.
+                            </p>
+                        </div>
+                        <div class="button">
+                            <button class="aboutMe">About Me</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="card swiper-slide">
+                    <div class="card-content">
+                        <div class="image">
+                            <img src="../my new logo.png">
+                        </div>
+                        <div class="about">
+                            <p>For a region hit hard by poverty, 
+                                genocide and disease, 
+                                the transformation is remarkable.
+                            </p>
+                        </div>
+                        <div class="button">
+                            <button class="aboutMe">About Me</button>
+                        </div>
+                    </div>
+                </div>
+               
+              </div>
+              <!-- <div class="swiper-pagination"></div> -->
+        <div class="swiper-button-prev"></div>
+        <div class="swiper-button-next"></div>
+            </div>
+        </div>
+    </section>
+    <section class="section">
+        <div class="container">
+            <div class="trending">
+                <div class="trnding_content">
+                    <h2 class="trending_title">Title Of Product</h2>
+                    <p class="trending_p">product trending display here with small content or description</p>
+                    <a href="#" class="trending_btn">CheckOut Now</a>
+                </div>
+                <img src="../MyOwnerLogo.png">
+            </div>
+        </div>
+    </section>
+    <section class="section">
+        <div class="container">
+            <div class="section_category">
+                <p class="section_category_p">Our Products</p>
+            </div>
+            <div class="section_header" style="background:none;box-shadow:none;color:black">
+                <h3 class="section_title" style="font-size:25px">Explore Our Products</h3>
+            </div>
+            <div class="products">
+                <div class="product_card">
+                    <div class="product_card_content">
+                        <div class="image">
+                            <img src="../my new logo.png">
+                        </div>
+                        <div class="about">
+                            <p>For a region hit hard by poverty, 
+                                genocide and disease, 
+                                the transformation is remarkable.
+                            </p>
+                        </div>
+                        <div class="button">
+                            <button class="aboutMe">About Me</button>
+                            <button class="hireMe">Hire Me</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="product_card">
+                    <div class="product_card_content">
+                        <div class="image">
+                            <img src="../my new logo.png">
+                        </div>
+                        <div class="about">
+                            <p>For a region hit hard by poverty, 
+                                genocide and disease, 
+                                the transformation is remarkable.
+                            </p>
+                        </div>
+                        <div class="button">
+                            <button class="aboutMe">About Me</button>
+                            <button class="hireMe">Hire Me</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="product_card">
+                    <div class="product_card_content">
+                        <div class="image">
+                            <img src="../my new logo.png">
+                        </div>
+                        <div class="about">
+                            <p>For a region hit hard by poverty, 
+                                genocide and disease, 
+                                the transformation is remarkable.
+                            </p>
+                        </div>
+                        <div class="button">
+                            <button class="aboutMe">About Me</button>
+                            <button class="hireMe">Hire Me</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="product_card">
+                    <div class="product_card_content">
+                        <div class="image">
+                            <img src="../my new logo.png">
+                        </div>
+                        <div class="about">
+                            <p>For a region hit hard by poverty, 
+                                genocide and disease, 
+                                the transformation is remarkable.
+                            </p>
+                        </div>
+                        <div class="button">
+                            <button class="aboutMe">About Me</button>
+                            <button class="hireMe">Hire Me</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="product_card">
+                    <div class="product_card_content">
+                        <div class="image">
+                            <img src="../my new logo.png">
+                        </div>
+                        <div class="about">
+                            <p>For a region hit hard by poverty, 
+                                genocide and disease, 
+                                the transformation is remarkable.
+                            </p>
+                        </div>
+                        <div class="button">
+                            <button class="aboutMe">About Me</button>
+                            <button class="hireMe">Hire Me</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="product_card">
+                    <div class="product_card_content">
+                        <div class="image">
+                            <img src="../my new logo.png">
+                        </div>
+                        <div class="about">
+                            <p>For a region hit hard by poverty, 
+                                genocide and disease, 
+                                the transformation is remarkable.
+                            </p>
+                        </div>
+                        <div class="button">
+                            <button class="aboutMe">About Me</button>
+                            <button class="hireMe">Hire Me</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="product_card">
+                    <div class="product_card_content">
+                        <div class="image">
+                            <img src="../my new logo.png">
+                        </div>
+                        <div class="about">
+                            <p>For a region hit hard by poverty, 
+                                genocide and disease, 
+                                the transformation is remarkable.
+                            </p>
+                        </div>
+                        <div class="button">
+                            <button class="aboutMe">About Me</button>
+                            <button class="hireMe">Hire Me</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    
+    <footer>
+        <div class="wrapper">
+            <div class="links-container">
+                <div class="links">
+                    <h3>Quick Links</h3>
+                    <ul>
+                        <li>About Us</li>
+                        <li>Contact Us</li>
+                        <li>Privacy Policy</li>
+                        <li>Terms & Conditions</li>
+                    </ul>
+                </div>
+                <div class="links">
+                    <h3>Our Courses</h3>
+                    <ul>
+                        <li>Login</li>
+                        <li>Download</li>
+                        <li>All Courses</li>
+                    </ul>
+                </div>
+                <div class="links">
+                    <h3>Quick Message</h3>
+                    <ul>
+                        <li>contact@gmail.com</li>
+                    </ul>
+                    <div class="social">
+                        <!-- <img src="images/logo.png"> -->
+                    </div>
+                    <form action="#">
+                        <input type="text" name="" placeholder="Email Address...">
+                        <button class="submit-btn">Send Me</button>
+                    </form>
+                </div>   
+            </div>
+            <p class="copyright">This website is developed by HHCoding © 2023</p>
+        </div>
+    </footer>
+
+
+
+
+    <script src="swiper.js"></script>
+    <script src="script.js"></script>
+    
+</body>
+</html>
---
+++ script.js
@@ -0,0 +1,65 @@
+var countDownDate = new Date("Dec 31, 2023 24:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("demo").innerHTML = days + " : " + hours + " : "
+  + minutes + " : " + seconds;
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
+// =========== swiper ===============
+var swiper = new Swiper(".swiper", {
+  slidesPerView: 1,
+  spaceBetween: 5,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false
+  },
+  navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+  breakpoints: {
+    "@0.00": {
+      slidesPerView: 1,
+      spaceBetween: 5,
+    },
+    "@0.75": {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    "@1.00": {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    "@1.50": {
+      slidesPerView: 4,
+      spaceBetween: 30,
+    },
+  },
+});
